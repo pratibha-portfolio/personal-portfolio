918daf6342524bda4e482aec80cef835121d9127
pratibha-portfolio <vinaykumar20102013@gmail.com>
2026-09-16 02:58:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let contactForm=document.getElementById("contactForm");
+if(contactForm){
+    contactForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    let name= document.getElementById("contactname").value.trim();
+    if(name===""){
+        alert("Please Enter your name!");
+        return;
+    }
+    alert("Message send Successfully!");
+    contactForm.reset();
+})
+}
+
+let themeButton=document.getElementById("themebutton");
+if(themeButton){
+themeButton.addEventListener("click", function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        themeButton.textContent="Light Mode";
+    }
+    else{
+        themeButton.textContent="Dark Mode";
+    }
+});
+}
+
+let scrollTopBtn=document.getElementById("scrollTopBtn");
+window.addEventListener("scroll", function(){
+    if(window.scrollY>300){
+        scrollTopBtn.style.display="block";
+    }else{
+        scrollTopBtn.style.display="none";
+    }
+});
+
+scrollTopBtn.addEventListener("click", function(){
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    });
+});
